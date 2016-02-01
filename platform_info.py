bf790f3c08824993b9206653915398bdb7d61197
guillermooo <guillermo.lopez@outlook.com>
2016-02-01 19:32:10 +0100

    refactor

---
+++ platform_info.py
@@ -0,0 +1,55 @@
+import sublime
+
+
+from .data import DataSection
+from .data import DataItem
+
+
+# Information about an OS.
+class PlatformInfo(DataSection):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__('Platform info', *args, **kwargs)
+
+    # Returns information about the currently running editor. This is the
+    # only public API.
+    @classmethod
+    def from_current_platform(cls):
+        info = SublimeTextInfo()
+        info.collect()
+        return info
+
+    # Indicates where the information was extracted from.
+    @property
+    def provider(self):
+        raise TypeError("don't instantiate %s" % self.__class__.__name__)
+
+    # TODO: Some providers will take long to generate data, so we need to notify the caller when
+    # we are done in some way; probably via events. Also, display some visual indication when
+    # some long-running op is in progress.
+    # Collects data about the editor.
+    def collect(self):
+        raise TypeError("don't instantiate %s" % self.__class__.__name__)
+
+
+# Information about Sublime Text.
+class SublimeTextInfo(EditorInfo):
+
+    def __init__(self):
+        super().__init__(description='General details about Sublime Text')
+
+    @classmethod
+    def from_current_editor(cls):
+        return self
+
+    @property
+    def provider(self):
+        return 'Sublime Text API'
+
+    def collect(self):
+        self.elements.clear()
+        self.elements.append(DataItem('name', 'Sublime Text'))
+        self.elements.append(DataItem('version', sublime.version()))
+        self.elements.append(DataItem('architecture', sublime.arch()))
+        self.elements.append(DataItem('channel', sublime.channel()))
+        self.elements.append(DataItem('platform', sublime.platform()))
