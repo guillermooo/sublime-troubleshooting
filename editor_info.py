59b67b4a24a68bae7eaf9a791db2277c8f92ee13
guillermooo <guillermo.lopez@outlook.com>
2016-02-01 21:07:27 +0100

    add info about active view

--- editor_info.py
+++ editor_info.py
@@ -4,6 +4,7 @@ import sublime
 
 from .data import DataSection
 from .data import DataItem
+from .data import DataBlock
 from .data import DataProvider
 
 
@@ -52,8 +53,31 @@ class SublimeTextInfo(EditorInfo):
 
     def collect(self):
         self.elements.clear()
-        self.elements.append(DataItem('name', 'Sublime Text'))
-        self.elements.append(DataItem('version', sublime.version()))
-        self.elements.append(DataItem('architecture', sublime.arch()))
-        self.elements.append(DataItem('channel', sublime.channel()))
-        self.elements.append(DataItem('platform', sublime.platform()))
+
+        db0 = DataBlock('Version and architecture')
+        db0.items.append(DataItem('name', 'Sublime Text'))
+        db0.items.append(DataItem('version', sublime.version()))
+        db0.items.append(DataItem('architecture', sublime.arch()))
+        db0.items.append(DataItem('channel', sublime.channel()))
+        db0.items.append(DataItem('platform', sublime.platform()))
+
+        view = sublime.active_window().active_view()
+        view_settings = view.settings()
+
+        db1 = DataBlock('View settings')
+        db1.items.append(DataItem('syntax', view_settings.get('syntax')))
+        db1.items.append(DataItem('tab_size', view_settings.get('tab_size')))
+        db1.items.append(DataItem('translate_tabs_to_spaces', view_settings.get('translate_tabs_to_spaces')))
+
+        db2 = DataBlock('View state')
+        db2.items.append(DataItem('is view dirty', view.is_dirty()))
+        db2.items.append(DataItem('is view readonly', view.is_read_only()))
+        db1.items.append(DataItem('encoding', view.encoding()))
+        db1.items.append(DataItem('em width', view.em_width()))
+        db1.items.append(DataItem('selection count', len(view.sel())))
+        db1.items.append(DataItem('has non empty selections', view.has_non_empty_selection_region()))
+        # db1.items.append(DataItem('is auto complete visible', view.is_auto_complete_visible()))
+
+        self.elements.append(db0)
+        self.elements.append(db1)
+        self.elements.append(db2)
