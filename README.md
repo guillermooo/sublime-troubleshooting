903a2f03ea24c0508deb2ed9abdc6fa7d7f467ff
guillermooo <guillermo.lopez@outlook.com>
2016-02-02 23:27:21 +0100

    add pybuilder script

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 A package for Sublime Text that helps you troubleshoot issues and generate complete error reports.
 
+---
+
 ### For developers
 
-Symlink (on Windows, create a directory junction) *./src* as *Data/Packages/Troubleshooting*.
+**All**, do this once (adjust as necessary):
 
-Symlink *./tests* as *Data/Packages/Troubleshootingtests*.
+```python
+$ python -m venv venv
+$ ./venv/Scripts/activate.ps1
+$ (venv) pip install -r requirements.txt
+```
 
-**TODO:** Add tasks to pybuilder to set this up.
+**For OS X or Linux**, symlink *./src* as *Data/Packages/Troubleshooting*, and *./tests* as *Data/Packages/Troubleshootingtests*.
+
+**For Windows**, run `pyb_ develop` once to have directory junctions set up automatically for you. Note that for any changes you make to the files, you'll have to restart Sublime Text to have them picked up.
--- build.py
+++ build.py
@@ -1,5 +1,13 @@
+import os
+import contextlib
+
+from subprocess import call
+
 from pybuilder.core import use_plugin
-from pybuilder import task
+from pybuilder.core import task
+
+
+SUBLIME_TEXT_DATA_PATH = os.environ.get('SUBLIME_TEXT_DATAX')
 
 # use_plugin("python.core")
 # use_plugin("python.unittest")
@@ -9,5 +17,42 @@ from pybuilder import task
 @task
 def greet():
     print("hello")
+
+
+@task
+def develop():
+    if os.name != 'nt':
+        print("not implemented for OS '%s'", os.name)
+        return
+
+    if not (SUBLIME_TEXT_DATA_PATH and os.path.exists(SUBLIME_TEXT_DATA_PATH)):
+        print(r"Can't locate the Data folder. Please set %SUBLIME_TEXT_DATA%.")
+        return
+
+    with cd(SUBLIME_TEXT_DATA_PATH) as old:
+        _make_junction(old)        
+
+
+@contextlib.contextmanager
+def cd(new):
+    old = os.getcwd()
+    os.chdir(new)
+    yield old
+    os.chdir(old)
+
+
+def _remove_junctions():
+    if os.path.exists('Troubleshooting'):
+        call(['rd', 'Troubleshooting'], shell=True)
+
+    if os.path.exists('Troubleshootingtests'):
+        call(['rd', 'Troubleshootingtests'], shell=True)
+
+
+def _make_junction(base):
+    _remove_junctions()
+    call(['cmd', '/c', 'mklink', '/J', 'Troubleshooting', os.path.join(base, 'src')], shell=True)
+    call(['cmd', '/c', 'mklink', '/J', 'Troubleshootingtests', os.path.join(base, 'tests')], shell=True)
+
 
 default_task = "greet"
