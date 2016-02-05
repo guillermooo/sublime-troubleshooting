2ea6a47acb1dadf63f85c1754218d9a4a8ebd4ad
guillermooo <guillermo.lopez@outlook.com>
2016-02-05 02:17:08 +0100

    present logging options in quick panel

--- src/commands/toggle_logging.py
+++ src/commands/toggle_logging.py
@@ -26,20 +26,33 @@ class ToggleLoggingCommand(sublime_plugin.WindowCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.states = defaultdict(lambda: False)
+        self.options = ['commands', 'input', 'commands+input', 'build systems', 'result regex', 'build systems+result regex']
+        self.actions = {
+            'commands': lambda x: sublime.log_commands(x),
+            'input': lambda x: sublime.log_input(x),
+            'build systems': lambda x: sublime.log_build_systems(x),
+            'result regex': lambda x: sublime.log_result_regex(x),
+        }
 
-    def run(self, kind):
-        state = self.states[kind]
-        if kind == 'commands':
-            sublime.log_commands(not state)
-        elif kind == 'input':
-            sublime.log_input(not state)
-        else:
-            show_status(self.window.active_view(),
-                        'ts.status', 'Troubleshooting: Unknown kind of logging: "{}"'
-                        .format(kind), duration=4000)
-            return
+    def run(self):
+        items = [[o, str(self.states[o]) if '+' not in o else '--'] for o in self.options]
+        self.window.show_quick_panel(items, self.toggle)        
 
-        self.states[kind] = not state
+    def toggle(self, index):
+        kinds = self.options[index]        
+        for kind in kinds.split('+'):            
+            try:
+                state = self.states[kind]
+                self.actions[kind](not state)
+            except KeyError:
+                show_status(self.window.active_view(),
+                            'ts.status', 'Troubleshooting: Unknown kind of logging: "{}"'
+                            .format(kind), duration=4000)
+                return
+            else:
+                self.states[kind] = not state
+
+        fragment = '; '.join('logging %s: {}' % item for item in kinds.split('+'))
         show_status(self.window.active_view(),
-                    'ts.status', 'Troubleshooting: Logging staus for "{}" set to "{}"'
-                    .format(kind, self.states[kind]), duration=4000)
+                    'ts.status', ('Troubleshooting: ' + fragment)
+                    .format(*[self.states[kind] for kind in kinds.split('+')]), duration=4000)
