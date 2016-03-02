042a194d6a510cb5762eb6ddd118fdb1e9c19731
guillermooo <guillermo.lopez@outlook.com>
2016-03-02 19:59:42 +0100

    fixes based on review + docstrings

--- plugin/platform_info.py
+++ plugin/platform_info.py
@@ -21,31 +21,52 @@ class PlatformInfo(DataProvider, DataSection):
     def __init__(self, *args, **kwargs):
         super().__init__('Platform info', *args, **kwargs)
 
-    # Returns information about the currently running editor. This is the
-    # only public API.
     @classmethod
     def from_current(cls):
+        """ Returns information about the currently running editor.
+
+        Use it as a factory method.
+        """
         plat = sys.platform
         if plat == 'win32':
             info = WindowsInfo()
         elif plat == 'darwin':
             info = OsxInfo()
         else:
-            info = UnixInfo()
+            info = LnuxInfo()
         return info
 
-    # Indicates where the information was extracted from.
+    @abc.abstractmethod
+    def collect_system_data(self):
+        """ Collects system data.
+
+        Subclasses must override this method.
+        """
+        pass
+
+    @abc.abstractmethod
+    def collect_display_data(self):
+        """ Collects display data.
+
+        Subclasses must override this method.
+        """
+        pass
+
+    def collect(self):
+        """ Collects all the platform data.
+
+        Subclasses need not but can override this method.
+        """
+        self.collect_system_data()
+        self.collect_display_data()
+
     @property
     @abc.abstractmethod
     def provider(self):
-        pass
+        """ Indicates where the information was extracted from.
 
-    # TODO: Some providers will take long to generate data, so we need to notify the caller when
-    # we are done in some way; probably via events. Also, display some visual indication when
-    # some long-running op is in progress.
-    # Collects data about the editor.
-    @abc.abstractmethod
-    def collect(self):
+        Subclasses must override this property.
+        """
         pass
 
 
@@ -73,10 +94,6 @@ class WindowsInfo(PlatformInfo):
         except Exception:
             _l.exception('unexpected exception while gathering data')
             raise
-
-    def collect(self):
-        self.collect_systeminfo_data()
-        self.collect_display_data()
 
     def collect_wmic_data(self, cmds, block_title):
         buf = []
@@ -116,7 +133,7 @@ class WindowsInfo(PlatformInfo):
 
         self.collect_wmic_data(cmds, 'Display Information')
 
-    def collect_systeminfo_data(self):
+    def collect_system_data(self):
         buf = []
 
         # TODO: add useful pieces of data / remove useless ones
@@ -155,17 +172,13 @@ class UnixInfo(PlatformInfo):
         except Exception as e:
             _l.debug('unexpected error while gathering data: %s', e)
 
-    def collect(self):
-        self.collect_uname_data()
-        self.collect_display_data()
-
     def collect_display_data(self):
         pass
 
-    def collect_uname_data(self):
+    def collect_system_data(self):
         buf = []
 
-        # TODO: Check if user can repeat keys fast
+        # TODO: Check if user can repeat keys fast (OS X)
         # TODO: Add information about useful tools like the step recorder
         # TODO: Generate purely informational status reports for users
 
@@ -202,6 +215,10 @@ class UnixInfo(PlatformInfo):
             db0.items.append(DataItem(*item.split('=')))
 
         self.elements.append(db0)
+
+
+class LinuxInfo(UnixInfo):
+    pass
 
 
 class OsxInfo(UnixInfo):
